1f0424871cbbf61203ba6ec15de5c804c818f053
Logan Demmy <frank.demmy@gmail.com>
2021-05-27 10:04:52 -0500

    restart the quest

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Quest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Adventuregame()
         {
             Console.Clear();
             // Create a few challenges for our Adventurer's quest
@@ -35,28 +35,28 @@ namespace Quest
 ",
                 4, 20
             );
-            Challenge coffee = new Challenge(@"
-                        What goes in coffee?
+            Challenge coffee = new Challenge(
+                @"What goes in coffee?
     1) Nothing
     2) Cream
     3) Sugar
 
-            ", 1, 40);
-            Challenge nss = new Challenge(@"
-                        Is NSS fun?
+", 1, 40);
+            Challenge nss = new Challenge(
+                @"Is NSS fun?
     1) Yah
     2) Nah
     3) Sugar
 
-            ", 3, 50);
-            Challenge ocean = new Challenge(@"
-                        Is NSS fun?
+", 3, 50);
+            Challenge ocean = new Challenge(
+                @"What is the best Ocean?
     1) Atlantic
     2) Pacific
     3) Indian
     4) Artic
 
-            ", 4, 20);
+", 4, 20);
             
 
             // "Awesomeness" is like our Adventurer's current "score"
@@ -70,10 +70,10 @@ namespace Quest
 
 
             Robe discription = new Robe();
-            // discription.Colors.Add("Red");
+            //  
             discription.Length = 30;
 
-            Console.Write($"What is your name adventurer?");
+            Console.Write($"What is your name adventurer? ");
             string adventurename = Console.ReadLine();
             // Make a new "Adventurer" object using the "Adventurer" class
             Adventurer theAdventurer = new Adventurer(adventurename, discription);
@@ -94,15 +94,21 @@ namespace Quest
             challenges.Add(favoriteBeatle, randomint());
             challenges.Add(coffee, randomint());
             challenges.Add(nss, randomint());
+            challenges.Add(ocean, randomint());
 
             var sorted = from pair in challenges
                  orderby pair.Value descending
                  select pair;
-
+            int i =0;
             // Loop through all the challenges and subject the Adventurer to them
             foreach (var challenge in sorted)
             {
+                if (i < 4)
+                {
+
                 challenge.Key.RunChallenge(theAdventurer);
+                }
+                i++;
             }
 
             // This code examines how Awesome the Adventurer is after completing the challenges
@@ -120,16 +126,18 @@ namespace Quest
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
             }
 
-            //Prompt user to play again
+        }
+        static void Main(string[] args){
+            Adventuregame();
+
             Console.WriteLine("This game is fun would you like to play again?!?");
             Console.Write("Y or N: ");
             string answer = Console.ReadLine().ToLower();
             if (answer == "y")
             {
-
+                Adventuregame();
             }
-        }
-
    
+        }
     }
 }
